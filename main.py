e7f2c2df300536162b71932643f12e345344124c
isabellegmuniz-png <isabelle.gmuniz@sempreceub.com>
2025-10-18 16:31:13 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Primeiro Projeto Acadêmico: Hello World Básico
+
+def saudacao(nome):
+    """Função que retorna uma saudação simples."""
+    return f"Olá, {nome}! Bem-vindo ao seu primeiro projeto no GitHub."
+
+if __name__ == "__main__":
+    usuario = "Isabelle"
+    mensagem = saudacao(usuario)
+    print(mensagem)
+
+# O objetivo deste projeto é apenas demonstrar o uso do GitHub/Git.
